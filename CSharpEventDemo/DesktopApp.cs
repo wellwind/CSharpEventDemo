93e8fefc079695d02ff71ad0303098ea88975239
wellwind <wellwind@mail2000.com.tw>
2016-05-22 09:49:46 +0800

    tweak event sample

--- CSharpEventDemo/DesktopApp.cs
+++ CSharpEventDemo/DesktopApp.cs
@@ -7,28 +7,16 @@ using TempatureMonitorLibs;
 
 namespace CSharpEventDemo
 {
-    public class DesktopApp : ITempatureMonitorObserver
+    public class DesktopApp : ITempatureMonitorObserver, ITempatureMonitorEvent
     {
-        private TempatureMonitorUsingEvent tempatureMonitor;
-
-        public DesktopApp()
-        {
-        }
-
-        public DesktopApp(TempatureMonitorUsingEvent tempatureMonitor)
-        {
-            this.tempatureMonitor = tempatureMonitor;
-            this.tempatureMonitor.OnTempatureChanged += TempatureMonitor_OnTempatureChanged;
-        }
-
-        private void TempatureMonitor_OnTempatureChanged(object sender, double e)
-        {
-            Console.WriteLine($"Desktop App使用事件委派方法得知溫度變化了: {e}");
-        }
-
         public void OnTempatureChanged(double tempature)
         {
             Console.WriteLine($"Desktop App被通知溫度變化了: {tempature}");
+        }
+
+        public void OnTempatureChangedEvent(object sender, double e)
+        {
+            Console.WriteLine($"Desktop App使用事件委派方法得知溫度變化了: {e}");
         }
     }
 }
--- CSharpEventDemo/Program.cs
+++ CSharpEventDemo/Program.cs
@@ -9,14 +9,23 @@ namespace CSharpEventDemo
 {
     internal class Program
     {
+        private static DesktopApp desktopApp = new DesktopApp();
+        private static MobileApp mobileApp = new MobileApp();
+
         private static void Main(string[] args)
+        {
+            obsverPatternDemo();
+
+            delegateDemo();
+
+            eventDemo();
+        }
+
+        private static void obsverPatternDemo()
         {
             // 使用一般Observer pattern
             Console.WriteLine("Observer Pattern Demo");
             var tempatureMonitor = new TempatureMonitorSubject();
-
-            var desktopApp = new DesktopApp();
-            var mobileApp = new MobileApp();
 
             tempatureMonitor.RegisterObserver(desktopApp);
             tempatureMonitor.RegisterObserver(mobileApp);
@@ -36,7 +45,10 @@ namespace CSharpEventDemo
             Console.WriteLine("溫度變化了，現在是27.6度");
             tempatureMonitor.Tempature = 27.6;
             Console.WriteLine();
+        }
 
+        private static void delegateDemo()
+        {
             // 使用Delegate完成Observer pattern
             Console.WriteLine("Delegate Demo");
             var tempatureMonitorDelegate = new TempatureMonitorUsingDelegate();
@@ -52,14 +64,25 @@ namespace CSharpEventDemo
 
             Console.WriteLine("溫度變化了，現在是28.6度");
             tempatureMonitorDelegate.Tempature = 28.6;
-            Console.WriteLine();
 
+            Console.WriteLine("mobileApp不再想觀察了");
+            tempatureMonitorDelegate.OnTempatureChanged -= mobileApp.OnTempatureChanged;
+
+            Console.WriteLine("溫度變化了，現在是27.6度");
+            tempatureMonitorDelegate.Tempature = 27.6;
+            Console.WriteLine();
+        }
+
+        private static void eventDemo()
+        {
             // 使用Event事件委派
             Console.WriteLine("Event Demo");
             var tempatureMonitorEvent = new TempatureMonitorUsingEvent();
 
-            desktopApp = new DesktopApp(tempatureMonitorEvent);
-            mobileApp = new MobileApp(tempatureMonitorEvent);
+            tempatureMonitorEvent.OnTempatureChanged += desktopApp.OnTempatureChangedEvent;
+            tempatureMonitorEvent.OnTempatureChanged += mobileApp.OnTempatureChangedEvent;
+            // 額外自訂事件委派方法
+            tempatureMonitorEvent.OnTempatureChanged += TempatureMonitorEvent_OnTempatureChanged;
 
             Console.WriteLine("溫度變化了，現在是30.5度");
             tempatureMonitorEvent.Tempature = 30.5;
@@ -69,6 +92,18 @@ namespace CSharpEventDemo
 
             Console.WriteLine("溫度變化了，現在是28.6度");
             tempatureMonitorEvent.Tempature = 28.6;
+
+            Console.WriteLine("mobileApp不再想觀察了");
+            tempatureMonitorEvent.OnTempatureChanged -= mobileApp.OnTempatureChangedEvent;
+
+            Console.WriteLine("溫度變化了，現在是27.6度");
+            tempatureMonitorEvent.Tempature = 27.6;
+            Console.WriteLine();
+        }
+
+        private static void TempatureMonitorEvent_OnTempatureChanged(object sender, double e)
+        {
+            Console.WriteLine($"自訂的委派方法得知溫度變化了: {e}");
         }
     }
 }
